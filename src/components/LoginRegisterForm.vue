833e614891b5349fa159b492c387f7d278c0a7c1
George Pickering <29524044+tedjenkins@users.noreply.github.com>
2019-04-03 11:19:43 +0100

    Rewrote and reintegrated LoginRegisterForm component

--- src/components/LoginRegisterForm.vue
+++ src/components/LoginRegisterForm.vue
@@ -1,192 +1,96 @@
 <template>
-  <form class="site-form" :id="formId" @submit.prevent="handleForm">
-    <h1>{{ formHeader }} {{ this.$siteName }}</h1>
-    <template v-for="element of elements">
-      <div class="form-section injected-html" v-if="element.html" v-html="element.html" :key="element.index" :id="element.id"></div>
-      <div class="form-section label-input-pairing" v-else-if="!element.html" :key="element.index">
-        <label :for="element.id">{{ element.label }} <span class="form-input-wordcount" style="display: none"></span></label> <input :type="element.type" minlength="6" maxlength="24" :id="element.id" @input="validateInput" @focus="validateInputFocus" @blur="validateInputBlur"/>
-      </div>
-    </template>
-  </form> 
+  <form class="site-form" :id="formId" @submit="handleForm">
+    <div class="form-section" v-for="element of elements" :key="elements.indexOf(element)" v-html="element.html"></div>
+  </form>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { isEmail } from 'validator';
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
 
-@Component({
-  components: {},
-  mounted() {
-    // Default textContent for each wordcount element is "(0/maxLength of input)"
-    document.querySelectorAll('.form-input-wordcount').forEach(node => node.textContent = `(0/${(node.parentElement!.nextSibling! as HTMLInputElement).maxLength})`);
-  }
-})
+@Component({})
 
 export default class LoginRegisterForm extends Vue {
-  @Prop() private formId!: string;
-  @Prop() private formHeader!: string;
-  @Prop() private elements!: object[];
+  @Prop() formId!: string;
+  @Prop() elements!: Array<{ id: string|undefined, html: string }>;
 
   /**
-   * Pre-submit validation. Displays current char length with max length beside input field (e.g. 33/128), adds / removes 'valid' class from field's <label>.
-   * @param {Event} e -- input event.
+   * Handle login / register form.
+   * @param {Event} e -- submit event.
    */
-  validateInput = (e: Event) => {
-    const targ = e.target as HTMLInputElement;
-    const targLabel = targ.previousElementSibling!;
+  handleForm(e: Event) {
+    e.preventDefault();
 
-    if (targ.type !== 'text') {
-      return;
-    }
-
-    // For email field, check if valid email address syntax
-    if (targ.id.includes('email')) {
-      if (isEmail(targ.value)) {
-        targLabel.classList.add('valid-input');
-      } else if (!isEmail(targ.value) && targLabel.classList.contains('valid-input')) {
-        targLabel.classList.remove('valid-input');
-      }
-    }
-
-    // Valid minimum length, don't check email fields
-    if (!targ.id.includes('email')) {
-      // Current length / maxLength count
-      targLabel.querySelector('.form-input-wordcount')!.textContent = `(${targ.value.length}/${targ.maxLength})`;
-
-      if (targ.value.length >= +targ.minLength) {
-        targLabel.classList.add('valid-input');
-      } else if (targ.value.length < +targ.minLength && targLabel.classList.contains('valid-input')) {
-        targLabel.classList.remove('valid-input');
-      }
-    }
-  }
-
-  /**
-   * Makes char length word count appear on input field focus.
-   * @param {Event} e -- focus event.
-   */
-  validateInputFocus = (e: Event) => {
-    const targ = e.target as HTMLInputElement;
-    const targLabel = targ.previousElementSibling!;
-
-    targLabel.querySelector('.form-input-wordcount')!.removeAttribute('style');
-  }
-
-  /**
-   * Makes char length word count disappear on input field blur.
-   * @param {Event} e -- blur event.
-   */
-  validateInputBlur = (e: Event) => {
-    const targ = e.target as HTMLInputElement;
-    const targLabel = targ.previousElementSibling!;
-
-    targLabel.querySelector('.form-input-wordcount')!.setAttribute('style', 'display: none');
-  }
-
-  /**
-   * Insert profile type value into hidden text field based on the profile type <div> clicked on.
-   * @param {MouseEvent} e -- mouse click event.
-   */
-  handleProfileType = (e: MouseEvent) => {
-    const targ = e.target as HTMLElement;
-    const hdnInput = targ.parentElement!.nextSibling as HTMLInputElement;
-
-    if (!targ.id.includes('form-profile-type')) {
-      hdnInput.value = targ.textContent!.toLowerCase();
-    }
-  }
-
-  /**
-   * Handle data in form on submit.
-   * @param {Event} e -- form submit event.
-   */
-  handleForm = (e: Event) => {
     const targ = e.target as HTMLFormElement;
-    console.log(targ);
+    const vals: string[] = [];
+    targ.querySelectorAll('input').forEach(el => {
+      if (el.type === 'password') {
+        const encrypted = btoa(el.value);
+        vals.push(encrypted);
+        return true;
+      }
+      if (el.value.length < 6) {
+        //
+      }
+      vals.push(el.value);
+    });
+
+    fetch(`${Vue.prototype.$siteHost}/register`, {
+      method: 'POST',
+      body: JSON.stringify(vals),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.log(err));
   }
 }
 </script>
 
-<style lang="scss" scoped>
-// Class to add to label elements, for marking input as success / valid.
-.valid-input {
-  color: forestgreen;
-}
+<style lang="scss">
+.site-form {
+  background-color: #fafafa;
+  border: 1px solid #ccc;
+  padding: 10px;
 
-.site-form-login-register {
-  display: grid;
-  grid-gap: 6px;
-  max-width: calc($site-max-width / 2);
+  .form-section {
+    padding: 4px;
 
-  h1 {
-    font-size: 20px;
-    margin-top: 10px;
-    text-align: center;
-  }
+    & > * {
+      display: block;
+      font-size: 18px;
+    }
 
-  /deep/ h2 {
-    font-size: 16px;
-  }
+    label {
+      font-weight: bold;
+      padding: 10px;
+      text-align: center;
+    }
 
-  label {
-    font-family: 'Montserrat', Helvetica, Arial, sans-serif;
-  }
-}
+    input {
+      margin: 0px auto;
+    }
 
-@media all and (min-width: 400px) {
+    button {
+      margin: 10px auto;
+    }
 
-}
-
-@media all and (min-width: 600px) {
-
-}
-
-@media all and (min-width: 800px) {
-
-}
-
-@media all and (min-width: 1200px) {
-  h1 {
-    font-size: 24px;
-    text-align: center;
-  }
-
-  .site-form-login-register {
-    .form-section {
-      align-items: center;
-      display: flex;
-      justify-content: center;
-      padding: 10px 0px;
-
-      label {
-        flex: 0 1 45%;
-      }
-
-      input {
-        flex: 0 1 55%;
-      }
-
-      button {
-        flex: 0 1 20%;
+    a {
+      display: inline;
+      
+      &:active {
+        outline-color: transparent;
       }
     }
 
-    .label-input-pairing {
-      flex-flow: column;
-
-      * {
-        font-size: 22px;
-      }
-
-      label {
-        font-weight: bold;
-      }
-
-      input {
-        margin-top: 10px;
-        width: 26%;
-      }
+    div {
+      text-align: center;
     }
+  }
+
+  #register-info {
+    text-align: center;
   }
 }
 </style>
--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -5,52 +5,8 @@
         <li class="li-selected" id="login-select">Login</li>
         <li id="register-select">Sign up</li>
       </ul>
-      <form class="site-form" id="login-form" v-if="showLogin" @submit="handleForm">
-        <div class="form-section">
-          <label for="login-email">Email address</label>
-          <input type="text" id="login-email" minlength="6" maxlength="24"/>
-        </div>
-        <div class="form-section">
-          <label for="login-password">Password</label>
-          <input type="password" id="login-password"/>
-        </div>
-        <div class="form-section" id="login-extra">
-          <button type="submit">Submit</button>
-          <div id="form-remember-me">
-            <input type="checkbox" id="form-remember"/> <label for="form-remember">Remember me</label>
-          </div>
-          <div id="form-forgot-password">
-            (<a href="#">forgot password?</a>)
-          </div>
-        </div>
-      </form>
-      <form class="site-form" id="register-form" v-if="!showLogin" @submit="handleForm">
-        <div class="form-section">
-          Signing up with {{ this.$siteName }} is quick and easy.
-        </div>
-        <div class="form-section">
-          <label for="register-username">Username</label>
-          <input type="text" id="register-username" minlength="6" maxlength="24"/>
-        </div>
-        <div class="form-section">
-          <label for="register-password">Password</label>
-          <input type="password" id="register-password"/>
-        </div>
-        <div class="form-section">
-          <label for="register-password-repeat">Repeat password</label>
-          <input type="password" id="register-password-repeat"/>
-        </div>
-        <div class="form-section">
-          <label for="register-email">Email address</label>
-          <input type="text" id="register-email" minlength="6" maxlength="24"/>
-        </div>
-        <div class="form-section" id="register-info">
-          By signing up you agree to our <a href="#">Terms of Service</a>, <a href="#">Privacy Policy</a> and our <a href="#">usage of cookies</a>. You must also be aged 13 or over to register.
-        </div>
-        <div class="form-section">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      <LoginRegisterForm formId="login-form" :elements="loginElements" v-if="showLogin"/>
+      <LoginRegisterForm formId="register-form" :elements="registerElements" v-if="!showLogin"/>
     </section>
     <section id="display-posts">
       here is where posts are displayed
@@ -62,6 +18,8 @@
 import { Component, Vue } from 'vue-property-decorator';
 import LoginRegisterForm from '@/components/LoginRegisterForm.vue';
 
+type formEls = Array<{ id: string|undefined, html: string }>;
+
 @Component({
   components: {
     LoginRegisterForm
@@ -69,6 +27,52 @@ import LoginRegisterForm from '@/components/LoginRegisterForm.vue';
 })
 
 export default class Home extends Vue {
+  loginElements: formEls = [
+    {
+      id: '',
+      html: `<label for="login-email">Email address</label> <input type="text" id="login-email" minlength="6" maxlength="24"/>`
+    },
+    {
+      id: '',
+      html: `<label for="login-password">Password</label> <input type="password" id="login-password"/>`
+    },
+    {
+      id: 'form-extra',
+      html: `<button type="submit">Submit</button> <div id="form-remember-me"> <input type="checkbox" id="form-remember"/> <label for="form-remember">Remember me</label></div> <div id="form-forgot-password">(<a href="#">forgot password?</a>)</div>`
+    }
+  ];
+
+  registerElements: formEls = [
+    {
+      id: '',
+      html: `Signing up with ${Vue.prototype.$siteName} is quick and easy.`
+    },
+    {
+      id: '',
+      html: `<label for="register-username">Username</label> <input type="text" id="register-username" minlength="6" maxlength="24"/>`
+    },
+    {
+      id: '',
+      html: `<label for="register-password">Password</label> <input type="password" id="register-password"/>`
+    },
+    {
+      id: '',
+      html: `<label for="register-password-repeat">Repeat password</label> <input type="password" id="register-password-repeat"/>`
+    },
+    {
+      id: '',
+      html: `<label for="register-email">Email address</label> <input type="text" id="register-email" minlength="6" maxlength="24"/>`
+    },
+    {
+      id: 'register-info',
+      html: `By signing up you agree to our <a href="#">Terms of Service</a>, <a href="#">Privacy Policy</a> and our <a href="#">usage of cookies</a>. You must also be aged 13 or over to register.`
+    },
+    {
+      id: '',
+      html: `<button type="submit">Submit</button>`
+    }
+  ];
+
   loggedIn: boolean = false;
   showLogin: boolean = true;
 
@@ -90,41 +94,7 @@ export default class Home extends Vue {
     targ.classList.add('li-selected');
 
     this.showLogin = (targ.textContent!.toLowerCase().includes('login')) ? true : false;
-  }
-
-  /**
-   * Handle login / register form.
-   * @param {Event} e -- submit event.
-   */
-  handleForm(e: Event) {
-    e.preventDefault();
-
-    const targ = e.target as HTMLFormElement;
-    const vals: string[] = [];
-    targ.querySelectorAll('input').forEach(el => {
-      if (el.type === 'password') {
-        const encrypted = btoa(el.value);
-        vals.push(encrypted);
-        return true;
-      }
-      if (el.value.length < 6) {
-        //
-      }
-      vals.push(el.value);
-    });
-
-    fetch(`${Vue.prototype.$siteHost}/register`, {
-      method: 'POST',
-      body: JSON.stringify(vals),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(err => console.log(err));
-  }
-}
+  }}
 </script>
 
 <style lang="scss" scoped>
@@ -151,51 +121,6 @@ export default class Home extends Vue {
       &:hover {
         background-color: white;
       }
-    }
-  }
-
-  .site-form {
-    background-color: #fafafa;
-    border: 1px solid #ccc;
-    padding: 10px;
-
-    .form-section {
-      padding: 4px;
-
-      & > * {
-        display: block;
-        font-size: 18px;
-      }
-
-      label {
-        font-weight: bold;
-        padding: 10px;
-        text-align: center;
-      }
-
-      input {
-        margin: 0px auto;
-      }
-
-      button {
-        margin: 10px auto;
-      }
-
-      a {
-        display: inline;
-        
-        &:active {
-          outline-color: transparent;
-        }
-      }
-
-      div {
-        text-align: center;
-      }
-    }
-
-    #register-info {
-      text-align: center;
     }
   }
 }
